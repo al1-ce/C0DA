a20469291bc9d6d710253f0be360ff342a11bf57
al1-ce <al1-ce@null.net>
2025-02-12 15:23:43 +0300

    add gitignore

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json())
 app.use(express.static(__dirname, { extensions: ["html", "htm"] }));
 
-app.all("*", (req, res) => { res.status(404).sendFile(path.join(__dirname, "404.html")); })
+app.all("*", (req, res) => { res.status(404).sendFile(path.join(__dirname, "/404.html")); })
 
 const port = process.env.PORT || 3000;
 
