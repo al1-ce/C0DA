fab8232bdbd06d503915389e543cd5bf84d83afa
al1-ce <al1-ce@null.net>
2025-02-12 15:32:02 +0300

    attemt fix pages

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.json())
-app.use(express.static(path.join(__dirname, "/public"), { extensions: ["html", "htm"] }));
+app.use(express.json());
+app.use(express.static( path.join(__dirname, "public"), { extensions: ["html", "htm"] }));
 
-app.all("*", (req, res) => { res.status(404).sendFile(path.join(__dirname, "/public/404.html")); })
+// app.get("/", (req, res) => serve_html(res, "../pages/main", "index.html"))
+
+// app.get("/test", (req, res) => res.send("Express test"));
+
+app.all("*", (req, res) => { res.status(404).sendFile(path.join(__dirname, "public/404.html")); })
 
 const port = process.env.PORT || 3000;
 
@@ -20,4 +24,3 @@ app.listen(port, (err, res) => {
 })
 
 module.exports = app;
-
