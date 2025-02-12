28dfadba73e7b6cfc757f2650c975e0aa710cd8e
al1-ce <al1-ce@null.net>
2025-02-12 15:35:05 +0300

    attemt fix pages 2

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-const express = require("express");
-const path = require("path");
+const express         = require("express");
+const path            = require("path");
 
 const app = express();
 
-app.use(express.json());
-app.use(express.static( path.join(__dirname, "public"), { extensions: ["html", "htm"] }));
-
-// app.get("/", (req, res) => serve_html(res, "../pages/main", "index.html"))
-
-// app.get("/test", (req, res) => res.send("Express test"));
-
-app.all("*", (req, res) => { res.status(404).sendFile(path.join(__dirname, "public/404.html")); })
+app.use( "/", express.static(path.join(__dirname, "public"), { extensions: ["html", "htm"] }));
+app.all("*", (_, res) => { res.status(404).sendFile(path.join(__dirname, "public/404.html")); });
 
 const port = process.env.PORT || 3000;
 
@@ -21,6 +15,7 @@ app.listen(port, (err, res) => {
     } else {
         console.log('[INFO] Server Running on port:', port);
     }
-})
+});
 
 module.exports = app;
+
