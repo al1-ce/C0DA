3cfd89d1c3f3fa79d8a74ffff8b9281b22ea3225
al1-ce <al1-ce@null.net>
2025-02-12 15:11:06 +0300

    fix html pages

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(__dirname, { extensions: ["html", "htm"] }));
+
+app.all("*", (req, res) => { res.status(404).sendFile(path.join(__dirname, "public/404.html")); })
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, (err, res) => {
+    if (err) {
+        console.log(err);
+        return res.status(500).send(err.message);
+    } else {
+        console.log('[INFO] Server Running on port:', port);
+    }
+})
+
+module.exports = app;
+
